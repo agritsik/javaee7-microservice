1a4e0d8e2cccbbd1780d2a8af3c16455f8af1fe1
andrey <a.gritsik@gmail.com>
2015-06-13 19:14:05 +0300

    added wildfly support

--- docker/run-package.sh
+++ docker/run-package.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-mvn package -DskipTests -Pdocker-dev
+mvn clean && mvn package -DskipTests -Pdocker-dev
 
 cp target/blog-1.0-SNAPSHOT.war docker/blog-gf/blog.war
+cp target/blog-1.0-SNAPSHOT.war docker/blog-wf/blog.war
--- src/test/java/com/agritsik/samples/blog/boundary/PostResourceTest.java
+++ src/test/java/com/agritsik/samples/blog/boundary/PostResourceTest.java
@@ -43,6 +43,7 @@ public class PostResourceTest extends TestCase {
         return ShrinkWrap.create(WebArchive.class, "test.war")
                 .addPackages(true, "com.agritsik")
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource("jbossas-ds.xml")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
