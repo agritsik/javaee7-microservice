7faba2621e538ebf2a8b0a4f4de99fcb8f66a252
andrey <a.gritsik@gmail.com>
2015-06-15 13:06:21 +0300

    switched to jdk8 wildfly docker

--- docker/run-wf.sh
+++ docker/run-wf.sh
@@ -5,4 +5,4 @@ docker build -t agritsik/blog-wf docker/blog-wf/
 docker rm -f -v blog-wf
 docker run -d -p 9990:9990 -p 9090:8080 --link=blog-db:blog-db --name blog-wf agritsik/blog-wf
 
-#docker logs -f blog-gf
+docker logs -f blog-wf
