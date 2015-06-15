06f8a1fbe02126c3bc369b618adc44a7f4602daf
andrey <a.gritsik@gmail.com>
2015-06-15 14:12:44 +0300

    fixed port for wildfly server

--- docker/run-wf.sh
+++ docker/run-wf.sh
@@ -3,6 +3,6 @@
 docker build -t agritsik/blog-wf docker/blog-wf/
 
 docker rm -f -v blog-wf
-docker run -d -p 9990:9990 -p 9090:8080 --link=blog-db:blog-db --name blog-wf agritsik/blog-wf
+docker run -d -p 9990:9990 -p 8080:8080 --link=blog-db:blog-db --name blog-wf agritsik/blog-wf
 
 docker logs -f blog-wf
