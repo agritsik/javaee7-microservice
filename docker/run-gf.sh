2c3e3394a123231fd4174370df771372e318fb9b
andrey <a.gritsik@gmail.com>
2015-08-19 18:59:37 +0300

    fixed war name

--- docker/run-gf.sh
+++ docker/run-gf.sh
@@ -5,4 +5,4 @@ docker build -t agritsik/blog-gf docker/blog-gf/
 docker rm -f -v blog-gf
 docker run -d -p 4848:4848 -p 8080:8080 --link=blog-db:blog-db --name blog-gf agritsik/blog-gf
 
-docker logs -f blog-gf
+#docker logs -f blog-gf
