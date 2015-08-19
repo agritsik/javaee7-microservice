1e5133d2b8a78e76b6e1565318945f8cbce84f52
andrey <a.gritsik@gmail.com>
2015-08-19 18:35:55 +0300

    switched to 1.0-beta-1 maven archetype version

--- src/main/java/com/agritsik/samples/blog/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.agritsik.samples.blog;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
